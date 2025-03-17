c9aeba9b01a07071d69b5457793c030b9698c6c8
Alex Morgan <jenovaris@gmail.com>
2025-03-17 14:48:55 -0400

    Finish update_dns and run methods. Create main method. Update exception handling. Update logging.

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 import os
 import requests
-import json
 import logging
-from typing import Dict, Any, Optional, Tuple
 
 # Logging config
 logging.basicConfig(
     filename="ddns.log",
     level=logging.INFO,
-    format="%(asctime)s    %(message)s",
+    # level=logging.DEBUG,
+    format="%(asctime)s    %(levelname)-8s    %(message)s",
     datefmt="%m-%d-%Y %H:%M:%S"
 )
 logger = logging.getLogger("ddns_client")
@@ -30,7 +29,11 @@ class DDNSClient:
         self.record_type = record_type
 
         if not self.api_key or not self.secret_api_key:
+            logger.error("API keys not in environment, exiting script")
             raise ValueError("API keys not found in environment variables")
+        else:
+            logger.debug(f"API keys found in environment\nAPIKEY: {self.api_key}\nSECRETAPIKEY: {self.secret_api_key}")
+
         
     def get_public_ip(self) -> str:
         """Get current public IP"""
@@ -48,7 +51,7 @@ class DDNSClient:
 
         payload = {
             "apikey": self.api_key,
-            "secretapikey": self.secret_api_key
+            "secretapikey": self.secret_api_key,
         }
 
         try:
@@ -61,37 +64,73 @@ class DDNSClient:
             response.raise_for_status()
         except requests.RequestException as e:
             logger.error(f"DNS check request failed: {e}")
-            return f"HTTP error: {str(e)}"
+            raise
         try: # Grab IP from api response
             ip_address = response.json()
             return ip_address['records'][0]['content']
         except KeyError as e:
             logger.error(f"DNS check content return failed at key: {e}")
-            return f"KeyValue error: {str(e)}"
+            raise
         
     def compare_ips(self, public_ip: str, dns_ip: str) -> bool:
         """Compare public IP with DNS entry IP, return true if same"""
         if public_ip == dns_ip:
-            logger.info(f"Public IP: {public_ip}, DNS IP: {dns_ip}. \
-                        Values match, not updating DNS")
             return True
         else:
-            logger.info(f"Public IP: {public_ip}, DNS IP: {dns_ip}. \
-                        Values differ, updating DNS")
             return False
             
-    def update_dns(self, public_ip: str) -> None:
-        pass
+    def update_dns(self, public_ip: str) -> str:
+        """Update DNS record with current public IP"""
+        url = f"{DNS_EDIT_ENDPOINT}/{DOMAIN}/{RECORD_TYPE}/"
+
+        payload = {
+            "apikey": self.api_key,
+            "secretapikey": self.secret_api_key,
+            "content": public_ip
+        }
+
+        try:
+            response = requests.post(
+                url,
+                headers={"Content-Type": "application/json"},
+                json=payload,
+                timeout=30
+            )
+            response.raise_for_status()
+            return response.text
+        except requests.RequestException as e:
+            logger.error(f"DNS edit request failed: {e}")
+            raise
 
 
     def run(self) -> None:
         public_ip = self.get_public_ip()
+        logger.info(f"Get public IP: {public_ip}")
         dns_ip = self.get_dns_record()
+        logger.info(f"Get DNS record IP: {dns_ip}")
+
+        try:
+            if self.compare_ips(public_ip, dns_ip):
+                logger.info(f"Public IP: {public_ip}, DNS IP: {dns_ip}    Values match, not updating DNS")
+                # logger.info("DNS record is up to date, exiting script.")
+                return
+            else:
+                logger.info(f"Public IP: {public_ip}, DNS IP: {dns_ip}    Values differ, updating DNS")
+                update_response = self.update_dns(public_ip)
+                logger.info(f"DNS record has been updated:    {public_ip}    {update_response}")
+                return
+        except:
+            logger.error("Unhandled exception")
+            raise
+
+
+def main(domain, record_type):
+    ddns_client = DDNSClient(domain, record_type)
+    ddns_client.run()
+    return
 
 if __name__ == "__main__":
-    pass
+    main(DOMAIN, RECORD_TYPE)
 
-# Finish update_dns method
-# Finish run method
-# Create main function
+
 # How can I test this code?
